9c2bc0ee4bbbf73661d8a9b7f04c59ad8336a24e
najoon <sk123928@gmail.com>
2022-03-21 13:42:13 +0900

    java5

---
+++ HelloKorea.java
@@ -0,0 +1,10 @@
+/*
+Mulity line this is comment
+*/
+// single line comment
+public class HelloKorea {
+	//commet
+	public static void main(String[] args) {
+		System.out.println("Hello korea");
+	}
+}
