59f347bb70756079dac1be3c291e575259c21cd0
najoon <sk123928@gmail.com>
2022-03-22 17:45:44 +0900

    java6

---
+++ Java_00_Hello/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,39 @@
+/*
+ * 현재 생성한 HelloKorea 클래스는
+ * com/callor/hello 폴더에 저장되어있다 라는 표식
+ * 현재 생성한 클래스가 저장된 base package 다 라고 한다.
+ * */
+
+package com.callor.hello;
+// 지금작성하는 소스코드는 HelloKoera.java 파일이다
+// HelloKorea.java 를 클래스파일 또는 자바소스코드 라고 한다.
+
+
+public class HelloKorea {
+//Syntax error 경고문 : 에러 키워드를 잘못 타이핑함
+	/**
+	 * main 키워드 + ctrl + space :상용구 작성
+	 * @param args
+	 * key word, reserved word, 명령어(command word)
+	 * package,public, class, static, void
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("반갑습니다");
+		System.out.println(30 + (40*2));
+		System.out.println(30 * 40);
+		System.out.println(40 - 30);
+		System.out.println(40 / 2);
+		
+		System.out.println("30 + 40 = " + (30+40));
+		
+		System.out.println("30+40="+"70");
+		// 문자열 덧셈 연산
+		// 문자열 + 문자열은 문자열을 연결하여 결과를 만든다
+		System.out.println("대한 : "+"민국");
+		System.out.println("대한민국");
+	}
+}
+
+
+
