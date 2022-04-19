5e34dd468b1f6ad9cf2a2501653333fe45a508cb
najoon <sk123928@gmail.com>
2022-04-19 16:43:42 +0900

    whi

---
+++ java_40_classes_03/src/com/callor/score/controller/ScoreContrlloerV1.java
@@ -0,0 +1,15 @@
+package com.callor.score.controller;
+
+import com.callor.score.service.ScoreServiceV1;
+
+public class ScoreContrlloerV1 {
+
+		public static void main(String[] args) {
+			String[] stNames = {"홍길동", "이몽룡", "성춘향", "임꺽정","장영자"};
+			ScoreServiceV1 scoreServiceV1 = new ScoreServiceV1(stNames);
+			scoreServiceV1.makeScore();
+			scoreServiceV1.printScore();
+			
+		}
+	
+}
