99e6eceb6e0a6d5b5a7f338a67b7079870251cb6
najoon <sk123928@gmail.com>
2022-04-20 20:15:06 +0900

    과제

---
+++ java_Daily/src/com/najoon/domain/ScoreVO.java
@@ -0,0 +1,42 @@
+package com.najoon.domain;
+
+public class ScoreVO {
+
+	private String name;
+	private int intSum;
+	private int intkor;
+	private int intMath;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getIntSum() {
+		return intSum;
+	}
+	public void setIntSum(int intSum) {
+		this.intSum = intSum;
+	}
+	public int getIntkor() {
+		return intkor;
+	}
+	public void setIntkor(int intkor) {
+		this.intkor = intkor;
+	}
+	public int getIntMath() {
+		return intMath;
+	}
+	public void setIntMath(int intMath) {
+		this.intMath = intMath;
+	}
+	public int getIntEng() {
+		return intEng;
+	}
+	public void setIntEng(int intEng) {
+		this.intEng = intEng;
+	}
+	private int intEng;
+	
+	
+}
