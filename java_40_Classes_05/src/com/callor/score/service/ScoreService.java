c459e80b2f244a1f2af9d7692c6a07d2be7fa573
najoon <sk123928@gmail.com>
2022-04-21 16:46:22 +0900

    객체

---
+++ java_40_Classes_05/src/com/callor/score/service/ScoreService.java
@@ -0,0 +1,11 @@
+package com.callor.score.service;
+
+public interface ScoreService {
+	//인터페이스
+	public void makeScore();
+	public void printScore();
+	
+	public int retSum();
+	public float retAvg();
+		
+}
