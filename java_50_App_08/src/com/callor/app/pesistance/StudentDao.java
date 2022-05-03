d4eefea93e0741b917cf3a645c0f0d0f2158ed3c
najoon <sk123928@gmail.com>
2022-05-03 17:02:46 +0900

    05023

---
+++ java_50_App_08/src/com/callor/app/pesistance/StudentDao.java
@@ -0,0 +1,46 @@
+package com.callor.app.pesistance;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.callor.app.dbconfig.DBConnection;
+import com.callor.app.dbconfig.DBContract;
+import com.callor.app.model.StudentVO;
+
+public class StudentDao {
+	
+	private final Connection dbConn;
+	public StudentDao() {
+		
+		dbConn = DBConnection.getDBConnection();
+		
+	}
+	public List<StudentVO> selectAll() throws SQLException{
+		PreparedStatement pStr = null;
+		String spl = DBContract.ST_SELECT;
+		pStr = dbConn.prepareStatement(spl);
+		ResultSet rSet = pStr.executeQuery();
+		
+		return getResult(rSet);
+		
+	}
+	private List<StudentVO> getResult(ResultSet rSet) throws SQLException{
+		List<StudentVO> stList = new ArrayList<>();
+		while(rSet.next()) {
+			StudentVO stVO = StudentVO.builder()
+						.stNum(rSet.getString(DBContract.ST_COL.ST_NUM))
+						.stName(rSet.getString(DBContract.ST_COL.ST_NAME))
+						.stDept(rSet.getString(DBContract.ST_COL.ST_DEPT))
+						.stGrade(rSet.getInt(DBContract.ST_COL.ST_GRADE))
+						.stAddr(rSet.getString(DBContract.ST_COL.ST_ADDR))
+						.stTel(rSet.getString(DBContract.ST_COL.ST_TEL))
+						.build();
+			stList.add(stVO);
+		}
+		return stList;
+	}
+}
